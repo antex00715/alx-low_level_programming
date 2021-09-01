7f93ac32407f197b5873d4201228daa22f762daa
antex man <ante3347kete@gmail.com>
2021-08-31 22:52:34 -0700

    fibonchi fix betty

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,28 +9,24 @@
 
 int main(void)
 {
-  int num1, num2, i, n;
-  int next_num;
-
-  n = 98;
-
-  for (i = 1; i <= n; i++)
-  {
-      if (i == 98)
-      {
-	  printf("%d, ", num2);
-      }
-      else
-      {
-	  printf("%d, ", num2);
-	  next_num = num1 + num2;
-	  num1 = num2;
-	  num2 = next_num;
-      }
-  }
-  printf("\n");
-
-  return (0);
+	int num1, num2, i, n;
+	int next_num;
+	
+	n = 98;
+	for (i = 1; i <= n; i++)
+	{
+		if (i == 98)
+		{
+			printf("%d, ", num2);
+		}
+		else
+		{
+			printf("%d, ", num2);
+			next_num = num1 + num2;
+			num1 = num2;
+			num2 = next_num;
+		}
+	}
+	printf("\n");
+	return (0);
 }
-
-
